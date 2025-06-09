75fa9f8274e8c484a0512d55d55a2da9ff4e9cae
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-06-08 23:18:15 -0300

    Create a context provider for managing the user account and sessions

---
+++ context/UserAccount.Provider.tsx
@@ -0,0 +1,62 @@
+import { getCurrentUser } from "@/services/appwrite";
+import { createContext, useContext, useEffect, useState } from "react";
+import { Models } from "react-native-appwrite";
+
+interface UserContext {
+  isLoggedIn: boolean;
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  userAccount: Models.Document | null;
+  setUserAccount: React.Dispatch<React.SetStateAction<Models.Document | null>>;
+  isLoading: boolean;
+}
+
+const UserContext = createContext<UserContext>({
+  isLoggedIn: false,
+  setIsLoggedIn: () => {},
+  userAccount: null,
+  setUserAccount: () => {},
+  isLoading: true,
+});
+
+export const UserAccountProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userAccount, setUserAccount] = useState<Models.Document | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getCurrentUser()
+      .then((res) => {
+        if (res) {
+          setUserAccount(res);
+          setIsLoggedIn(true);
+        } else {
+          setUserAccount(null);
+          setIsLoggedIn(false);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <UserContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        userAccount,
+        setUserAccount,
+        isLoading,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => useContext(UserContext);
