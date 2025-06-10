4845b5769d55725998814d2b60ed7417b5f2ff07
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-06-09 23:50:27 -0300

    Create a types folder

---
+++ types/videoPost.ts
@@ -0,0 +1,14 @@
+export interface AppwriteFile {
+  name: string;
+  uri: string;
+  type: string;
+  size: number;
+}
+
+export interface VideoPost {
+  title: string;
+  thumbnail: string;
+  videoUrl: string;
+  description: string;
+  creator: string;
+}
