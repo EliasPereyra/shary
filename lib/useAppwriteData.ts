9f6a54821149fc85247910929cc48b8fc5f604c1
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-06-11 20:30:39 -0300

    Add types for the func param of the appwrite data hook

--- lib/useAppwriteData.ts
+++ lib/useAppwriteData.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Models } from "react-native-appwrite";
 
 /**
  * El hook es para hacer un fetch y refetch de los datos, ofreciendo tambien la posibilidad de ver si estan cargando los datos.
@@ -6,15 +7,17 @@ import { useEffect, useState } from "react";
  * @param fn
  * @returns data, isLoading, reFetchData
  */
-export const useAppwriteData = (fn: () => Promise<any>) => {
-  const [data, setData] = useState([]);
+export const useAppwriteData = (
+  fn: () => Promise<Models.Document[] | undefined>
+) => {
+  const [data, setData] = useState<Models.Document[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     setIsLoading(true);
     try {
       const res = await fn();
-      setData(res);
+      setData(res!);
     } catch (error) {
       throw new Error("Error al obtener los datos", {
         cause: error,
