fb1eb9baeda28072b7d4bf6e41bc869f23cf2d80
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-06-13 17:18:38 -0300

    Implement the sign-in operation

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,28 +1,61 @@
+import { useState } from "react";
+import { Alert, StyleSheet } from "react-native";
+import { Link, router } from "expo-router";
+
 import Button from "@/components/atoms/button";
 import Input from "@/components/atoms/input";
 import AuthTemplate from "@/components/templates/auth";
 import { ThemedText } from "@/components/ThemedText";
 import { Colors } from "@/constants/Colors";
-import { Link } from "expo-router";
-import { StyleSheet, Text } from "react-native";
+import { getCurrentUser, logIn } from "@/services/appwrite";
+import { useUserContext } from "@/context/UserAccount.Provider";
 
 export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const { redirect } = styles;
+  const { setUserAccount, setIsLoggedIn } = useUserContext();
+
+  const submit = async () => {
+    setIsLoading(true);
+    try {
+      await logIn(email, password);
+      const user = await getCurrentUser();
+      setUserAccount(user);
+      setIsLoggedIn(true);
+
+      Alert.alert("Bienvenido!");
+      router.replace("/home");
+    } catch (error: any) {
+      Alert.alert("Error al iniciar sesión", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <AuthTemplate>
-      <Input type="text" label="Email" />
-      <Input type="text" label="Contraseña" />
-      <Text style={styles.forgotPassword}>Olvidé mi contraseña</Text>
+      <Input type="email" label="Email" onChangeText={setEmail} value={email} />
+      <Input
+        type="password"
+        label="Contraseña"
+        onChangeText={setPassword}
+        value={password}
+      />
+      <ThemedText style={styles.forgotPassword}>
+        Olvidé mi contraseña
+      </ThemedText>
       <Button
-        href="/(tabs)"
+        isLoading={isLoading}
+        onPress={submit}
         bg={Colors.light.primary}
         color={Colors.light.white}
       >
         Iniciar sesión
       </Button>
       <Link style={redirect} href="/signup">
-        <ThemedText type="default">
+        <ThemedText type="default" lightColor={Colors.light.lightGray}>
           ¿No tienes una cuenta?{" "}
           <ThemedText type="bold" style={{ color: Colors.light.black }}>
             Regístrate
