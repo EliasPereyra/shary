2b23f9d8f2beefbc69ce23ee0e51820848a772e5
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-06-22 23:42:58 -0300

    Improve the styles

--- components/atoms/no-videos-msg.tsx
+++ components/atoms/no-videos-msg.tsx
@@ -1,13 +1,22 @@
-import { Image, Text, View } from "react-native";
+import { Image, View } from "react-native";
+
+import { ThemedText } from "../ThemedText";
+import { Colors } from "@/constants/Colors";
 
 export default function NoVideosMessage() {
   return (
-    <View style={{ display: "flex", flexDirection: "column" }}>
+    <View style={{ padding: 16, display: "flex", flexDirection: "column" }}>
       <Image
         source={require("@/assets/images/search-off.png")}
-        style={{ width: 20, height: 20 }}
+        style={{ width: 60, height: 60, marginBottom: 8 }}
       />
-      <Text>No se encontraron vídeos</Text>
+      <ThemedText
+        type="default"
+        darkColor={Colors.light.black}
+        style={{ marginBottom: 16 }}
+      >
+        No se encontraron vídeos. Crea uno y comparte con los demás.
+      </ThemedText>
     </View>
   );
 }
